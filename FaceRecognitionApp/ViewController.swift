2b547c553c677b052dfe3a599c5bc4912b93f68e
tunaytoksoz <tunaytoksoz@hotmail.com>
2022-07-23 14:03:42 +0300

    faceID recognition

---
+++ FaceRecognitionApp/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  FaceRecognitionApp
+//
+//  Created by Tunay Toksöz on 23.07.2022.
+//
+
+import UIKit
+import LocalAuthentication
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func signInClicked(_ sender: Any) {
+        
+        let authContext = LAContext()
+        
+        var error: NSError?
+        
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is it you?") { succes, error in
+                if succes == true {
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "toSecondVC", sender: nil)
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        self.myLabel.text = "Error"
+                    }
+                }
+            }
+        }
+        
+        
+    }
+    
+}
+
